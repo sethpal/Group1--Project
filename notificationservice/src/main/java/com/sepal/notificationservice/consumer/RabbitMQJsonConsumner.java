562a4d4ccee75039e3d82ce2b7f0db3555a8572b
Seth Pal <sethpalster@gmail.com>
2023-11-05 23:15:23 +0530

    added notification service

--- notificationservice/src/main/java/com/sepal/notificationservice/consumer/RabbitMQJsonConsumner.java
+++ notificationservice/src/main/java/com/sepal/notificationservice/consumer/RabbitMQJsonConsumner.java
@@ -1,31 +1,98 @@
 package com.sepal.notificationservice.consumer;
 
-import com.sepal.notificationservice.dtos.User;
+import com.sepal.notificationservice.dtos.ConsumerResponseDto;
+import com.sepal.notificationservice.dtos.NotificationRequestDto;
+import com.sepal.notificationservice.dtos.NotificationResponseDto;
+import com.sepal.notificationservice.dtos.UserDto;
+import com.sepal.notificationservice.models.Notification;
+import com.sepal.notificationservice.models.Status;
+import com.sepal.notificationservice.models.UserContact;
+import com.sepal.notificationservice.repositories.NotificationRepository;
+import com.sepal.notificationservice.repositories.UserRepository;
 import com.sepal.notificationservice.services.SendEmailService;
+import com.sepal.notificationservice.services.SendSMSService;
+import lombok.Data;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
-import org.springframework.boot.context.event.ApplicationReadyEvent;
-import org.springframework.context.event.EventListener;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
+@Data
 public class RabbitMQJsonConsumner {
     private static final Logger LOGGER= LoggerFactory.getLogger(RabbitMQJsonConsumner.class);
-    SendEmailService sendEmailService;
+    private final SendEmailService sendEmailService;
+    private final UserRepository userRepository;
+    private  String notificationStatus;
+    private final NotificationRepository notificationRepository;
 
-    public RabbitMQJsonConsumner(SendEmailService sendEmailService) {
+    private final SendSMSService sendSMSService;
+    @Value("${spring.mail.username}")
+    private String fromMail;
+    @Value("${spring.mail.subject}")
+    private String emailSubject;
+
+
+    public RabbitMQJsonConsumner(SendEmailService sendEmailService
+                                 ,SendSMSService sendSMSService
+                                 ,UserRepository userRepository
+                                 ,NotificationRepository notificationRepository)
+    {
         this.sendEmailService = sendEmailService;
+        this.sendSMSService=sendSMSService;
+        this.userRepository=userRepository;
+        this.notificationRepository=notificationRepository;
+
     }
 
     //Consumer is subscribed to the Queue
     @RabbitListener(queues = {"${rabbitmq.queue.json.name}"})
-    public void consumeJsonMessage(User user){
-        LOGGER.info(String .format("Json Message Received --> %s", user.toString()));
-        String to="sethpalster@gmail.com";
-        String sub="Test";
-        String body="Hi This is just a test mail";
-        sendEmailService.sendEmail(to,user.getFirstName(),user.getLastName());
+    public void consumeJsonMessage(NotificationRequestDto notificationDto){
+        LOGGER.info(String .format("Json Message Received --> %s", notificationDto.toString()));
+
+        /*
+        In request body I will receive a user id
+        For that user id I will fetch the user contact (email address and mobile number) from the user data table
+        Send sms and email to those contacts
+        */
+
+        UserDto userDto=new UserDto();
+        NotificationResponseDto notificationResponseDto=new NotificationResponseDto();
+        ConsumerResponseDto consumerResponseDto=new ConsumerResponseDto();
+
+        Optional<UserContact> userOptional;
+        userOptional = userRepository.findById(Long.valueOf(notificationDto.getUserid()));
+
+        //Check if user is present in DB or not
+        if(userOptional.isEmpty()){
+            notificationStatus= String.valueOf(Status.FAILED);
+            LOGGER.info("User with "+notificationDto.getUserid()+" not present in DB");
+        }
+        else {
+            userDto.setUserid(userOptional.get().getId());
+            userDto.setEmail(userOptional.get().getEmail());
+            userDto.setMobile(userOptional.get().getMobile());
+
+            notificationResponseDto=sendEmailService.sendEmail(fromMail, userDto.getEmail(), emailSubject, notificationDto.getMessage());
+            consumerResponseDto.setEmailStatus(notificationResponseDto.getStatus());
+            String toNumber = "+917417331773"; // this need to be updated once i fetch the user mobile from the db
+            //toNumber=userDto.getMobile(); //enable this when reading data from user table
+            notificationResponseDto=sendSMSService.sendSMS(toNumber, notificationDto.getMessage());
+            consumerResponseDto.setMobileStatus(notificationResponseDto.getStatus());
+            Notification notification = new Notification();
+            notification.setMessageDetails(notificationDto.getMessage());
+            notification.setStatus((Status.DELIVERED));
+            notification.setUser_id(notificationDto.getUserid());
+            notificationRepository.save(notification);
+
+            notificationStatus = String.valueOf(Status.DELIVERED);
+        }
+
     }
+
+
 
 }
--- notificationservice/src/main/java/com/sepal/notificationservice/models/Notification.java
+++ notificationservice/src/main/java/com/sepal/notificationservice/models/Notification.java
@@ -1,8 +1,21 @@
 package com.sepal.notificationservice.models;
 
-public class Notification extends BaseModel{
-    String messageDetails;
-    String notificationStatus;
-   // NotificationType
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+import java.util.Collection;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Notification extends BaseModel{
+
+    private String messageDetails;
+    private Status status;
+    private Long user_id;
 }
