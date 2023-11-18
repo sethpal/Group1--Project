a349b7e92b038d1c555019697d16a2acf8fccd9f
Seth Pal <sethpalster@gmail.com>
2023-11-19 01:02:42 +0530

    latest code
    latest code

--- notificationservice/src/main/java/com/sepal/notificationservice/NotificationserviceApplication.java
+++ notificationservice/src/main/java/com/sepal/notificationservice/NotificationserviceApplication.java
@@ -4,6 +4,7 @@ package com.sepal.notificationservice;
 import com.twilio.Twilio;
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,7 +12,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import com.sepal.notificationservice.config.TwilioConfig;
 import org.springframework.context.annotation.Configuration;
 
-@SpringBootApplication()
+import java.util.Base64;
+
+@SpringBootApplication
 @Configuration
 public class NotificationserviceApplication {
 
@@ -20,6 +23,7 @@ public class NotificationserviceApplication {
 
 	@Value("${twilio.authToken}")
 	private String auth;
+
 	public static void main(String[] args) {
 		SpringApplication.run(NotificationserviceApplication.class, args);
 	}
@@ -27,7 +31,18 @@ public class NotificationserviceApplication {
 	@PostConstruct
 	public void setup()
 	{
+		Twilio.init(decodeValue(accountSSID),decodeValue(auth));
+	}
+	public String decodeValue(String encodedValue)
+	{
+		byte[] decodedBytes = Base64.getDecoder().decode(encodedValue);
+		return new String(decodedBytes);
+	}
 
-		Twilio.init(accountSSID,auth);
+	public String  encodeValue(String encodedValue) {
+		String BasicBase64format
+				= Base64.getEncoder()
+				.encodeToString(encodedValue.getBytes());
+		return BasicBase64format;
 	}
 }
